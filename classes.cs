a10b72961d11bc7515449a0e2ba47e82129ec8ea
ashongwe <abel.shongwe@structureit.net>
2020-02-16 15:28:08 +0200

    adding classes tutorial updated constructor overloading

--- classes.cs
+++ classes.cs
@@ -1,17 +1,28 @@
+using System;
+
 namespace Classes
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Overloading constructors
             Customer c1 = new Customer("Seefeesaw","Shongwe");
             c1.PrintFullNmae();
+            Customer c2 = new Customer();
+            c2.PrintFullNmae();
         }
     }
     class Customer
     {
         string _firstName;
         string _lastName;
+
+        //Parameterless Constructor
+        public Customer() : this("No firstName Provide","No lastName Provided")
+        {
+
+        }
 
         public Customer(string firstName,string lastName)
         {
